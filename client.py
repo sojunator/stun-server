9d692c9380bd6c0d84f3172e9adefacbe40e39ce
Carl Söyseth <carl.soyseth@octacon.se>
2021-07-01 15:20:36 +0200

    Configured client for Lukas

--- client.py
+++ client.py
@@ -26,24 +26,27 @@ def punchThrough(address, sock):
 
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 client_socket.settimeout(1.0)
-request = b'83.73.233.41'
-addr = ("127.0.0.1", 12000)
+request = b'89.233.217.40'
+addr = ("104.248.28.213", 12000)
+mode = "punch"
 
 t = Thread(target=holdPortOpenToSTUN, args=(addr, client_socket))
 t.start()
+
+print("Talking to:{}".format(addr[0]))
+
 client_socket.sendto(request, addr)
+if mode == "punch":
+    try:
+        request = request.decode("utf-8")
+        data, server = client_socket.recvfrom(1024)
+        messageString = data.decode("utf-8")
 
-try:
-    request = request.decode("utf-8")
-    data, server = client_socket.recvfrom(1024)
-    messageString = data.decode("utf-8")
-
-    if messageString == "404":
-        print("Host could not resolve this")
-    else:
-        print("Host told me to contact via: {}:{}".format(request, messageString))
-        punchThrough((request, int(messageString)), client_socket)
-except socket.timeout:
-    print('REQUEST TIMED OUT')
-
+        if messageString == "404":
+            print("Host could not resolve this")
+        else:
+            print("Host told me to contact via: {}:{}".format(request, messageString))
+            punchThrough((request, int(messageString)), client_socket)
+    except socket.timeout:
+        print('REQUEST TIMED OUT')
 
