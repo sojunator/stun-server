2aed824e00c27db81803dcd4bfce62eec2e77edd
Carl Söyseth <carl.soyseth@octacon.se>
2021-07-01 14:58:31 +0200

    first commit

---
+++ client.py
@@ -0,0 +1,49 @@
+import time
+import socket
+
+from threading import Thread
+punched = False
+def holdPortOpenToSTUN(addr, socket): 
+    while not punched:
+        socket.sendto(b'pulse', addr)
+        print("maintaining the port open")
+
+        time.sleep(5)
+
+def punchThrough(address, sock):  
+    print("attempting to punch through to: {}".format(address)) 
+
+    while not punched:
+        sock.sendto(b'hi mark', address)
+        try:
+            sock.recvfrom(1024) 
+        except socket.timeout:
+            print('REQUEST TIMED OUT')
+
+ 
+
+
+
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.settimeout(1.0)
+request = b'83.73.233.41'
+addr = ("127.0.0.1", 12000)
+
+t = Thread(target=holdPortOpenToSTUN, args=(addr, client_socket))
+t.start()
+client_socket.sendto(request, addr)
+
+try:
+    request = request.decode("utf-8")
+    data, server = client_socket.recvfrom(1024)
+    messageString = data.decode("utf-8")
+
+    if messageString == "404":
+        print("Host could not resolve this")
+    else:
+        print("Host told me to contact via: {}:{}".format(request, messageString))
+        punchThrough((request, int(messageString)), client_socket)
+except socket.timeout:
+    print('REQUEST TIMED OUT')
+
+
---
+++ server.py
@@ -0,0 +1,33 @@
+import random
+import socket
+
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+server_socket.bind(('', 12000))
+
+conn_dict = {"83.73.233.41" : "44455" }
+
+while True: 
+    message, address = server_socket.recvfrom(1024)
+    message = message.decode("utf-8") 
+
+    if message == "pulse":
+        print("Heartbeat from: {}".format(address))
+        conn_dict[address[0]] = address[1]
+
+    elif message == "register":
+        print("Hello from: {}".format(address))
+        conn_dict[address[0]] = address[1]
+    else:
+
+        print("{}:{} is requesting a connection to {}".format(address[0], address[1], message))
+
+        #add the requester to the list 
+        ipLookUp = message
+     
+
+        if ipLookUp in conn_dict.keys():
+            server_socket.sendto(bytearray(conn_dict[ipLookUp], "utf8"), address)
+            print("resolved it")
+        else:
+            server_socket.sendto(b'404', address)
+            print("I could not resolve this")
